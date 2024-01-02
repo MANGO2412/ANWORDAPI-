9022e78cf1863d22bcffb60101ba9abe3239fc1f
MANGO2412 <manuelgom3z22052003@gmail.com>
2024-01-01 22:50:17 -0800

    change configuration

--- src/Models/DB.js
+++ src/Models/DB.js
@@ -7,7 +7,9 @@ config();
 const db=new Sequelize(process.env.DB,process.env.USER,process.env.PASS,{
     host:process.env.HOST,
     dialect:'postgres',
-    ssl:true
+    ssl:{
+        rejectUnauthorized: true,
+    }
 })
 
 
